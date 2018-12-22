db12e09a5e901b11fd7515fc6c6eb029be104a35
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-12-22 20:27:14 +0100

    refactor

--- src/States/TD.js
+++ src/States/TD.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import assert from 'assert';
 
 import State from './State';
 import Population from '../Population';
@@ -7,6 +6,10 @@ import GenotypeBlueprint from '../GenotypeBlueprint';
 
 class TDState extends State
 {
+    constructor(sceneWrapper) {
+        super(sceneWrapper);
+    }
+
     init() {
         const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
         this.scene.add(directionalLight);
@@ -28,94 +31,100 @@ class TDState extends State
         * 1111 1111|1111 1111|1111 1111|0000 1000|0000 1000|0000 1000
         */
 
-       const nbPerRow = 5;
-       const cellsize = 750;
+       this.nbPerRow = 5;
+       this.cellsize = 750;
 
-       const cubeBlueprint = new GenotypeBlueprint();
+       this.cubeBlueprint = new GenotypeBlueprint();
 
-       cubeBlueprint.addTrait('r', 8, GenotypeBlueprint.INTEGER, 255, value => value / 255);
-       cubeBlueprint.addTrait('g', 8, GenotypeBlueprint.INTEGER, 0, value => value / 255);
-       cubeBlueprint.addTrait('b', 8, GenotypeBlueprint.INTEGER, 0, value => value / 255);
-       cubeBlueprint.addTrait('width', 8, GenotypeBlueprint.INTEGER, 16);
-       cubeBlueprint.addTrait('height', 8, GenotypeBlueprint.INTEGER, 16);
-       cubeBlueprint.addTrait('depth', 8, GenotypeBlueprint.INTEGER, 16);
+       this.cubeBlueprint.addTrait('r', 8, GenotypeBlueprint.INTEGER, 255, value => value / 255);
+       this.cubeBlueprint.addTrait('g', 8, GenotypeBlueprint.INTEGER, 128, value => value / 255);
+       this.cubeBlueprint.addTrait('b', 8, GenotypeBlueprint.INTEGER, 0, value => value / 255);
+       this.cubeBlueprint.addTrait('width', 8, GenotypeBlueprint.INTEGER, 16);
+       this.cubeBlueprint.addTrait('height', 8, GenotypeBlueprint.INTEGER, 16);
+       this.cubeBlueprint.addTrait('depth', 8, GenotypeBlueprint.INTEGER, 16);
 
-       cubeBlueprint.updateTargetModel();
-       
-        const show = (population) => {
-            const group = new THREE.Group();
-            group.shouldBeDeletedOnCleanUp = true;
-    
-            population.genotypes.forEach((genotype, i) => {
-                const data = cubeBlueprint.decode(genotype);
-                
-                const geometry = new THREE.BoxGeometry(data.width, data.height, data.depth);
-                const material = new THREE.MeshLambertMaterial({
-                    color: new THREE.Color(data.r, data.g, data.b), 
-                    transparent: true, 
-                    opacity: 1.0
-                });
+       this.cubeBlueprint.updateTargetModel();
 
-                const cube = new THREE.Mesh(geometry, material);
-                cube.castShadow = true;
-                cube.receiveShadow = true;
-    
-                const row = Math.floor(i / nbPerRow);
-                const col = i % nbPerRow;
+        this.loop = true;
+        this.population = new Population(20, this.cubeBlueprint.size);
+        this.population.evaluate(this.cubeBlueprint);
 
-                const x = col * cellsize;
-                const y = population.generation * 750;
-                const z = row * cellsize;
+        this.generationLoop();
+    }
 
-                cube.position.set(x, y, z);
+    generationLoop() {
+        if (this.loop) {
+            setTimeout(() => {
+                this.population.generation++;
 
-                if (population.size <= 1 || genotype.score === 0) {
-                    this.wrapper.camera.position.set(x, y, z);
-                }
-                
-                group.add(cube);
-            });
-    
-            //new THREE.Box3().setFromObject(group).getCenter(group.position).multiplyScalar(-1);
-            this.scene.add(group);
+                this.show(this.population);
+                this.generate();
+                this.generationLoop();
+            }, 500);
         }
+    }
+    generate () {
+        this.population.evaluate(this.cubeBlueprint);
 
-        let loop = true;
-        let population = new Population(20, cubeBlueprint.size);
-        population.evaluate(cubeBlueprint);
+        const selection = this.population.selectBestCandidates(Math.floor(this.population.size() * 0.5));
 
-        const generate = () => {
-            show(population);
-            population.generation++;
-            population.evaluate(cubeBlueprint);
-
-            const selection = population.selectBestCandidates(Math.floor(population.size() * 0.5));
-
-            if (population.hasTarget(cubeBlueprint) || selection.length <= 0) {
-                loop = false;
-            }
-            else {
-                // children
-                for (let i = 0, n = selection.length; i < n; i += 2) {
-                    if (selection[i] && selection[i + 1]) {
-                        const child = selection[i].crossWith(selection[i + 1]);
-                        selection.push(...child);
-                    }
+        if (this.population.hasTarget(this.cubeBlueprint) || selection.length <= 0) {
+            this.loop = false;
+        }
+        else {
+            // children
+            for (let i = 0, n = selection.length; i < n; i += 2) {
+                if (selection[i] && selection[i + 1]) {
+                    const child = selection[i].crossWith(selection[i + 1]);
+                    selection.push(...child);
                 }
-
-                // mutate
-                population.genotypes = selection.map(genotype => genotype.mutate(0.005));
             }
-            generationLoop();
-        };
 
-        const generationLoop = () => {
-            if (loop) {
-                setTimeout(() => generate(), 500);
+            // mutate
+            this.population.genotypes = selection.map(genotype => genotype.mutate(0.005));
+        }
+    }
+
+    /**
+     * Place current population on a grid in 3d
+     * @param {Population} population
+     */
+    show(population) {
+        const group = new THREE.Group();
+        group.shouldBeDeletedOnCleanUp = true;
+
+        population.genotypes.forEach((genotype, i) => {
+            const data = this.cubeBlueprint.decode(genotype);
+            
+            const geometry = new THREE.BoxGeometry(data.width, data.height, data.depth);
+            const material = new THREE.MeshLambertMaterial({
+                color: new THREE.Color(data.r, data.g, data.b), 
+                transparent: true, 
+                opacity: 1.0
+            });
+
+            const cube = new THREE.Mesh(geometry, material);
+            cube.castShadow = true;
+            cube.receiveShadow = true;
+
+            const row = Math.floor(i / this.nbPerRow);
+            const col = i % this.nbPerRow;
+
+            const x = col * this.cellsize;
+            const y = population.generation * 750;
+            const z = row * this.cellsize;
+
+            cube.position.set(x, y, z);
+
+            if (population.size() <= 1 || genotype.score === 0) {
+                this.wrapper.camera.position.set(x, y, z);
             }
-        };
+            
+            group.add(cube);
+        });
 
-        generationLoop();
+        // new THREE.Box3().setFromObject(group).getCenter(group.position).multiplyScalar(-1);
+        this.scene.add(group);
     }
     update() { }
 }
