267ac7cf685cd9b2f9e24dd0eae8641c0234eeab
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-01-24 20:23:05 +0100

    genetic cube test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import 'reset-css';
 import './assets/sass/style.scss';
 
 import SceneWrapper from './SceneWrapper';
+import CubeState from './States/CubeState';
 import ChairState from './States/ChairState';
 
 const scene = new SceneWrapper();
+const cubeState = new CubeState(scene);
 const chairState = new ChairState(scene);
 
 scene.init();
+scene.addState(cubeState);
 scene.addState(chairState);
-scene.start(0);
+scene.start(1);
