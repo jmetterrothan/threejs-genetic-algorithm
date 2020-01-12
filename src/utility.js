b25e84457da029023d872699a8f4c06a3900b08e
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-01-12 01:38:49 +0100

    feat: new shuffle array func

--- src/utility.js
+++ src/utility.js
@@ -1,37 +1,47 @@
 const TWO_PI = Math.PI * 2;
 
-const degToRad = (deg) => {
-    return deg * Math.PI / 180;
+const degToRad = deg => {
+  return (deg * Math.PI) / 180;
 };
 
-const radToDeg = (rad) => {
-    return rad * 180 / Math.PI;
+const radToDeg = rad => {
+  return (rad * 180) / Math.PI;
 };
 
 const lerp = (a, b, t) => {
-    return (1 - t) * a + t * b;
+  return (1 - t) * a + t * b;
 };
 
 /**
  * Modulo function that handles negative values
- * @param {number} x 
- * @param {number} m 
+ * @param {number} x
+ * @param {number} m
  * @return number
  */
 const mod = (x, m) => {
-    return (x % m + m) % m;
+  return ((x % m) + m) % m;
 };
 
-const shuffleArray = arr => arr
-  .map(a => [Math.random(), a])
-  .sort((a, b) => a[0] - b[0])
-  .map(a => a[1]);
+/**
+ * @param {Array} a
+ * @return Array
+ */
+const shuffleArray = a => {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+};
 
 export default {
-    TWO_PI,
-    degToRad,
-    radToDeg,
-    lerp,
-    mod,
-    shuffleArray,
+  TWO_PI,
+  degToRad,
+  radToDeg,
+  lerp,
+  mod,
+  shuffleArray
 };
