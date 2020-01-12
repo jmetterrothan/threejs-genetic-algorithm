6470176a5d8ab6714281f4abddafa22b73944360
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2020-01-12 12:18:48 +0100

    feat: remove index cubestate

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import 'reset-css';
 import './assets/sass/style.scss';
 
 import SceneWrapper from './SceneWrapper';
-import CubeState from './States/CubeState';
 import ChairState from './States/ChairState';
 
 const scene = new SceneWrapper();
-const cubeState = new CubeState(scene);
 const chairState = new ChairState(scene);
 
 scene.init();
-scene.addState(cubeState);
 scene.addState(chairState);
-scene.start(1);
+scene.start(0);
